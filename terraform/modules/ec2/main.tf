e0f369b0aa0f519de427c0e9f6c184fb65436449
RohitMahipalTTN <rohit.mahipal@tothenew.com>
2021-10-17 22:04:14 +0530

    sg1

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 // CUSTOM AMI CREATION...
 resource "aws_ami_from_instance" "alpha_public_ami" {
-  source_instance_id = var.instance_id
   name      = "${var.environment}-LEMP-Server AMI"
+  source_instance_id = var.instance_id
   tags          = {
     Name      = "${var.environment}-LEMP-Server AMI"
     Use      = "Devops Bootcamp"
@@ -10,7 +10,7 @@ resource "aws_ami_from_instance" "alpha_public_ami" {
 
 //EC2-INSTANCE CREATION...
 resource "aws_instance" "alpha_public_instance" {
-  ami           = aws_ami_from_instance.alpha_public_ami # us-east-1
+  ami           = aws_ami_from_instance.alpha_public_ami.id # us-east-1
   instance_type = var.instance_type
   subnet_id     = var.public_subnet_id
   key_name      = var.key_name
